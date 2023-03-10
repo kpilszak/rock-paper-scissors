a6ea1374eb8e34a8517526f8a2d64caf558f0bae
kpilszak <klaudia.pilszak@gmail.com>
2023-03-10 15:37:56 -0400

    Add calculating result logic

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const resultDisplay = document.querySelector('#result');
 const choicesDisplay = document.querySelector('#choices');
 const choices = ['rock', 'paper', 'scissors'];
 
-const handleClick = () => {
-    console.log('clicked');
+const handleClick = (e) => {
+    getResults(e.target.innerHTML, choices[Math.floor(Math.random() * choices.length)]);
 }
 
 choices.forEach(choice => {
@@ -12,3 +12,23 @@ choices.forEach(choice => {
     button.addEventListener('click', handleClick);
     choicesDisplay.appendChild(button);
 })
+
+const getResults = (userChoice, computerChoice) => {
+    switch(userChoice + computerChoice) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU WIN!';
+            break;
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , YOU LOSE!';
+            break;
+        case 'scissorsscissors':
+        case 'rockrock':
+        case 'paperpaper':
+            resultDisplay.innerHTML = 'You chose ' + userChoice + ' and the computer chose ' + computerChoice + ' , ITS A DRAW!';
+            break;
+    }
+}
