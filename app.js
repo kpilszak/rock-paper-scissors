da8ec0b8cb532bd37c40550c8d391422834d11ad
kpilszak <klaudia.pilszak@gmail.com>
2023-03-13 15:28:35 -0400

    Display choices

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const resultDisplay = document.querySelector('#result');
 const choicesDisplay = document.querySelector('#choices');
 const choices = ['rock', 'paper', 'scissors'];
+
+const handleClick = () => {
+    console.log('clicked');
+}
+
+choices.forEach(choice => {
+    const button = document.createElement('button');
+    button.innerHTML = choice;
+    button.addEventListener('click', handleClick);
+    choicesDisplay.appendChild(button);
+})
